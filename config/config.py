f976a962c25f507e28625778a47a77c081815b99
Jack <jack.ac02@gmail.com>
2024-08-12 00:00:05 -0600

    Actualizar config.py

--- config/config.py
+++ config/config.py
@@ -21,7 +21,7 @@ import os
 class Envs:
 	# appId = int("APP_ID") #Debe ir en formato int 
 	appId = int(os.environ.get("APP_ID")) 
-	username = urllib.parse.quote_plus(os.getenv("USERNAME"))
+	username = urllib.parse.quote_plus(os.getenv("USER_NAME"))
 	password = urllib.parse.quote_plus(os.getenv("PASS_KEY"))
 	MongoUrl = f"mongodb+srv://{username}:{password}@bot0.qdusz.mongodb.net/"
 	Owner = "@jack_dev02"
