be7a9af2a2ae3943a895cc399b666ab7eb1b9fdb
Jack <jack.ac02@gmail.com>
2024-08-14 13:35:27 -0600

    Update config.py

--- config/config.py
+++ config/config.py
@@ -21,7 +21,7 @@ load_dotenv()
 
 class Envs:
 	# appId = int("APP_ID") #Debe ir en formato int 
-	appId = int(os.environ.get("APP_ID")) 
+	appId = int(os.getenv.get("APP_ID")) 
 	username = urllib.parse.quote_plus(os.getenv("USER_NAME"))
 	password = urllib.parse.quote_plus(os.getenv("PASS_KEY"))
 	MongoUrl = f"mongodb+srv://{username}:{password}@bot0.qdusz.mongodb.net/"
